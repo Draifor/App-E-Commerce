45a2367175de7de0f13bbde1026cb29b102b49ce
Alfredo Parra <alfredo-920@hotmail.com>
2023-01-21 21:12:50 -0500

    Config Flowbite css library

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,18 @@
-import './globals.css'
+import './styles/globals.css'
+import Header from './components/header'
 
-export default function RootLayout({ children }) {
+export default function RootLayout ({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
