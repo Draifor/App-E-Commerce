6e57c3e0bfa68f8b3a770f23fd3e1d7f97a037e9
Alfredo Parra <alfredo-920@hotmail.com>
2023-01-28 01:19:36 +0000

    Commit for test codesandbox environment

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,18 @@
-import './styles/globals.css'
-import Header from './components/header'
+import "./styles/globals.css";
+import Header from "./components/Header";
 
-export default function RootLayout ({ children }) {
+export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className='dark'>
+      <body className="dark">
         <Header />
         {children}
       </body>
     </html>
-  )
+  );
 }
